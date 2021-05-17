9c0de747e74446501a4d7a33bf6c67bcda51fc6a
JJLindsey <jlindsey010@gmail.com>
2021-05-17 16:28:34 -0500

    build layout

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,15 @@
-var getUsersRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUsersRepos = function (user) {
+    //format the githib api url
+    var apiUrl = "http://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+        console.log(data);
+        });
+    });
 };
+
+console.log("outside");
 
 getUsersRepos();
